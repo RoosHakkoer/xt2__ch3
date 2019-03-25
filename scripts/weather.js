74818c8a9e32964e996335007f262df3786a88f0
Roos Hakkoer <rosaline93ha@gmail.com>
2019-03-25 17:13:19 +0100

    Combined API's

---
+++ scripts/weather.js
@@ -0,0 +1,37 @@
+
+function getAPIweather() {
+
+	fetch('http://api.openweathermap.org/data/2.5/weather?appid=b0c8dafa512a0134e90df6ece3c2b7a2&q=the%20Hague,nl&units=metric').then(function(response) {
+		return response.json();
+	})
+ 	.then(function(response) {
+ 		console.log(response.main.temp);
+
+		var temp = Math.round(response.main.temp, 0);
+		//var request = ;
+
+		if(temp <=10){
+			//show temp
+			document.getElementById('weather').innerHTML = temp + ' °C ';
+			//show message
+			document.getElementById('message').innerHTML = "It is cold outside"+", "+"enjoy this image while landing";
+			//show cat picture
+			showCatPicture();
+		}
+		else {
+			//show temp
+			document.getElementById('weather').innerHTML = temp + ' degrees';
+			//show message
+			document.getElementById('message').innerHTML = "The weather is pleasant";
+			//don't show cat picture
+			// show sun picture
+		}
+
+ 	})
+	.catch(function (error) {
+	 	console.log('Request failed', error);
+	});
+
+}
+//aanroepen
+getAPIweather();
